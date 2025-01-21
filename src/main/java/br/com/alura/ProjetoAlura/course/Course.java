4f653f773e660a2afa70a4f017903c36ef583b85
Davs <123772756+davsteixeira@users.noreply.github.com>
2025-01-21 00:34:09 -0300

    feat: added the Course entity and the status enumeration

---
+++ src/main/java/br/com/alura/ProjetoAlura/course/Course.java
@@ -0,0 +1,93 @@
+package br.com.alura.ProjetoAlura.course;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import java.time.LocalDateTime;
+
+@Entity(name="Course")
+@Table(name = "Course")
+public class Course {
+
+    @Id
+    @Column(length = 10)
+    private String code;
+
+    private String name;
+
+    private String description;
+
+    @Email
+    private String instructorEmail;
+
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.ACTIVE;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    private LocalDateTime inactivationAt;
+
+    public Course() {}
+
+    public Course(String code, String name, String description, String instructorEmail) {
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.instructorEmail = instructorEmail;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getInstructorEmail() {
+        return instructorEmail;
+    }
+
+    public void setInstructorEmail(String instructorEmail) {
+        this.instructorEmail = instructorEmail;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getInactivationAt() {
+        return inactivationAt;
+    }
+
+    public void setInactivationAt(LocalDateTime inactivationAt) {
+        this.inactivationAt = inactivationAt;
+    }
+}
